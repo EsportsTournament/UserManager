7140d2a1f2924482f4b4cacc9aafbcb25f10aea3
aditya-thakur <adityathakur532@gmail.com>
2022-06-26 19:04:48 +0530

    login works

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID ||
+  "888992868623-g5ffjm72bs7sli419ksdq44iinhml7kv.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET =
+  process.env.GOOGLE_CLIENT_SECRET || "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";
+export const SERVER_ROOT_URI = "http://localhost:4000";
+export const UI_ROOT_URI = "http://localhost:3000";
+export const JWT_SECRET = "open_token";
+export const COOKIE_NAME = "auth_token";
