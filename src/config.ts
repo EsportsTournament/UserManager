2f41218da38d8a375e67b493217a228d0eec6a71
aditya-thakur <adityathakur532@gmail.com>
2022-07-14 21:08:22 +0530

    bug fixes

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const GOOGLE_CLIENT_ID =
   "888992868623-g5ffjm72bs7sli419ksdq44iinhml7kv.apps.googleusercontent.com";
 export const GOOGLE_CLIENT_SECRET =
   process.env.GOOGLE_CLIENT_SECRET || "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";
-export const SERVER_ROOT_URI = "https://user-manager-meta.herokuapp.com/";
-export const UI_ROOT_URI = "https://www.metclan.xyz/";
+export const SERVER_ROOT_URI = "https://user-manager-meta.herokuapp.com"; // Does not ends with '/'
+export const UI_ROOT_URI = "https://www.metclan.xyz"; // Does not ends with '/'
 export const JWT_SECRET = "open_token";
 export const COOKIE_NAME = "auth_token";
